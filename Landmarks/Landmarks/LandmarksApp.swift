1b812577974557e834797a50cc31a6066a73f955
Tori <ttang1135@naver.com>
2023-01-30 15:11:21 +0900

    Swift 튜토리얼 1장-1

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 방유빈 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
